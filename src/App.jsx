9d8ecbcced250f95f8861f6267790fc5bc263fe1
jncruz-dev <jncruz_dev@outlook.com>
2025-01-12 12:24:26 +0100

    Lab 1: Add course titles

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Diplomado en fullstack</h2>
+      <h3>Modulo 7: Curso de React</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
