de79203233ad7d1972becef0473000406df792cd
jncruz-dev <jncruz_dev@outlook.com>
2025-01-13 07:14:51 +0100

    Components part 1

---
+++ src/components/Home/Body.jsx
@@ -0,0 +1,15 @@
+const Body = () => {
+  return (
+    <div className="card">
+      <h1>Vite + React</h1>
+      <h2>Diplomado en fullstack</h2>
+      <h3>Modulo 7: Curso de React</h3>
+      
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+    </div>
+  )
+}
+
+export default Body;
