3292116fe64fadf7858c1cb73326fb2db0512133
jncruz-dev <jncruz_dev@outlook.com>
2025-01-15 19:20:19 +0100

    Lab 6: Add counter to Home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,10 +5,14 @@ import Footer from "../components/Home/Footer"
 import viteLogo from "../assets/vite.svg"
 import reactLogo from "../assets/react.svg"
 import gitHubLogo from "../assets/github.svg"
+import { useSelector } from "react-redux"
 
 const Home = () => {
+  const count = useSelector((state) => state.counter.value);
+
   return (
     <div>
+      <span>{count}</span>
       <div>
         <OpenLink
           redirectURL="https://vite.dev"
